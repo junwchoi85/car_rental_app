4fff4d9c20faf18a1c19f6a3597ce9a013c8f6db
Jun Hwan <junwchoi85@gmail.com>
2024-09-15 09:34:19 +1200

    on boarding screen

--- lib/src/on_boarding/presentation/views/on_boarding_screen.dart
+++ lib/src/on_boarding/presentation/views/on_boarding_screen.dart
@@ -1,9 +1,13 @@
 import 'package:car_rental_app/core/common/views/loading_view.dart';
 import 'package:car_rental_app/core/common/widgets/gradient_background.dart';
+import 'package:car_rental_app/core/extensions/context_extension.dart';
 import 'package:car_rental_app/core/resources/media_res.dart';
+import 'package:car_rental_app/src/on_boarding/domain/entities/page_content.dart';
 import 'package:car_rental_app/src/on_boarding/presentation/cubit/on_boarding_cubit.dart';
+import 'package:car_rental_app/src/on_boarding/presentation/widget/on_boarding_body.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnBoardingScreen extends StatefulWidget {
   const OnBoardingScreen({super.key});
@@ -15,6 +19,8 @@ class OnBoardingScreen extends StatefulWidget {
 }
 
 class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  final pageController = PageController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +33,39 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
                 state is CachingFirstTimer) {
               return const LoadingView();
             }
-            return const Text('OnBoarding Screen');
+            return Stack(
+              children: [
+                PageView(
+                  controller: pageController,
+                  children: const [
+                    OnBoardingBody(pageContent: PageContent.first()),
+                    OnBoardingBody(pageContent: PageContent.second()),
+                    OnBoardingBody(pageContent: PageContent.third()),
+                  ],
+                ),
+                Align(
+                  alignment: const Alignment(0, .18),
+                  child: SmoothPageIndicator(
+                    controller: pageController,
+                    count: 3,
+                    onDotClicked: (index) {
+                      pageController.animateToPage(
+                        index,
+                        duration: const Duration(milliseconds: 500),
+                        curve: Curves.easeInOut,
+                      );
+                    },
+                    effect: const WormEffect(
+                      dotHeight: 10,
+                      dotWidth: 10,
+                      spacing: 40,
+                      // activeDotColor: Colours.primaryColour,
+                      dotColor: Colors.grey,
+                    ),
+                  ),
+                ),
+              ],
+            );
           },
           listener: (context, state) {
             if (state is OnBoardingStatus && !state.isFirstTimer) {
---
+++ lib/src/on_boarding/presentation/widget/on_boarding_body.dart
@@ -0,0 +1,64 @@
+import 'package:car_rental_app/core/extensions/context_extension.dart';
+import 'package:car_rental_app/src/on_boarding/domain/entities/page_content.dart';
+import 'package:flutter/material.dart';
+
+class OnBoardingBody extends StatelessWidget {
+  const OnBoardingBody({super.key, required this.pageContent});
+
+  final PageContent pageContent;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          pageContent.image,
+          height: context.height * .4,
+        ),
+        SizedBox(height: context.height * .03),
+        Padding(
+          padding: const EdgeInsets.all(20).copyWith(bottom: 0),
+          child: Column(
+            children: [
+              Text(
+                pageContent.title,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: context.height * .02),
+              Text(
+                pageContent.description,
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontSize: 14),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 50,
+                    vertical: 17,
+                  ),
+                  // backgroundColor: Colours.primaryColour,
+                  foregroundColor: Colors.white,
+                ),
+                onPressed: () {
+                  // context.read<OnBoardingCubit>().cacheFirstTimer();
+                },
+                child: const Text(
+                  'Get Started',
+                  style: TextStyle(
+                      // fontFamily: Fonts.aeonik,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
+                ),
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
