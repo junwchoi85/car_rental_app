92f2cbf0fb74077d47bc1cec8551dd93f35495b7
Jun Hwan <junwchoi85@gmail.com>
2024-09-21 07:23:39 +1200

    dashboard screen

--- lib/src/dashboard/presentation/views/dashboard_screen.dart
+++ lib/src/dashboard/presentation/views/dashboard_screen.dart
@@ -1,5 +1,11 @@
+import 'package:car_rental_app/core/common/app/providers/user_provider.dart';
 import 'package:car_rental_app/core/extensions/context_extension.dart';
+import 'package:car_rental_app/src/auth/data/models/user_model.dart';
+import 'package:car_rental_app/src/dashboard/providers/dashboard_controller.dart';
+import 'package:car_rental_app/src/dashboard/utils/dashboard_utils.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
@@ -12,16 +18,86 @@ class Dashboard extends StatefulWidget {
 
 class _DashboardState extends State<Dashboard> {
   @override
+  void initState() {
+    super.initState();
+
+    /// Set the orientation of the screen to portrait
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Placeholder(
-      child: Center(
-        child: Text(
-          'DASHBOARD',
-          style: context.theme.textTheme.bodyLarge?.copyWith(
-            color: Colors.white,
-          ),
-        ),
-      ),
+    return StreamBuilder<LocalUserModel>(
+      stream: DashboardUtils.userDataStream,
+      builder: (_, snapshot) {
+        if (snapshot.hasData && snapshot.data is LocalUserModel) {
+          context.read<UserProvider>().user = snapshot.data;
+        }
+        return Consumer<DashboardController>(builder: (_, controller, __) {
+          return Scaffold(
+            body: IndexedStack(
+              index: controller.currentIndex,
+              children: controller.screens,
+            ),
+            bottomNavigationBar: BottomNavigationBar(
+              currentIndex: controller.currentIndex,
+              showSelectedLabels: false,
+              backgroundColor: Colors.white,
+              elevation: 8,
+              onTap: controller.changeIndex,
+              items: [
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.home,
+                    color: controller.currentIndex == 0
+                        ? context.theme.primaryColor
+                        : Colors.grey,
+                    size: controller.currentIndex == 0 ? 30 : 24,
+                  ),
+                  label: 'Home',
+                  backgroundColor: Colors.white,
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.description_rounded,
+                    color: controller.currentIndex == 1
+                        ? context.theme.primaryColor
+                        : Colors.grey,
+                    size: controller.currentIndex == 1 ? 30 : 24,
+                  ),
+                  label: 'Home',
+                  backgroundColor: Colors.white,
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.chat,
+                    color: controller.currentIndex == 2
+                        ? context.theme.primaryColor
+                        : Colors.grey,
+                    size: controller.currentIndex == 2 ? 30 : 24,
+                  ),
+                  label: 'Home',
+                  backgroundColor: Colors.white,
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.account_circle,
+                    color: controller.currentIndex == 3
+                        ? context.theme.primaryColor
+                        : Colors.grey,
+                    size: controller.currentIndex == 3 ? 30 : 24,
+                  ),
+                  label: 'Home',
+                  backgroundColor: Colors.white,
+                ),
+              ],
+            ),
+          );
+        });
+      },
     );
   }
 }
