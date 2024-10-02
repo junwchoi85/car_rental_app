e11eb5ea7ba0d5e760c294ff25b0ffe590583d2a
Jun Hwan <junwchoi85@gmail.com>
2024-10-02 15:44:16 +1300

    Adding controller

--- lib/src/car/presentation/refactors/car_hire_body.dart
+++ lib/src/car/presentation/refactors/car_hire_body.dart
@@ -1,3 +1,4 @@
+import 'package:car_rental_app/core/common/widgets/custom_time_picker.dart';
 import 'package:car_rental_app/core/extensions/context_extension.dart';
 import 'package:car_rental_app/core/resources/media_res.dart';
 import 'package:car_rental_app/core/services/injection_container.dart';
@@ -15,6 +16,66 @@ class CarHireBody extends StatefulWidget {
 }
 
 class _CarHireBodyState extends State<CarHireBody> {
+  final TextEditingController _pickUpLocationController =
+      TextEditingController();
+  final TextEditingController _dropOffLocationController =
+      TextEditingController();
+  final TextEditingController _pickUpTimeController = TextEditingController();
+  final TextEditingController _dropOffTimeController = TextEditingController();
+  DateTime? _pickUpDate;
+  DateTime? _dropOffDate;
+  TimeOfDay? _pickUpTime;
+  TimeOfDay? _dropOffTime;
+
+  Future<void> _selectDate(BuildContext context, bool isPickUp) async {
+    final DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime.now(),
+      lastDate: DateTime(2101),
+    );
+
+    if (pickedDate != null) {
+      setState(() {
+        if (isPickUp) {
+          _pickUpDate = pickedDate;
+        } else {
+          _dropOffDate = pickedDate;
+        }
+      });
+    }
+  }
+
+  void _selectTime(BuildContext context, bool isPickUp) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return CustomTimePicker(
+          onTimeSelected: (TimeOfDay selectedTime) {
+            setState(() {
+              final formattedTime = selectedTime.format(context);
+              if (isPickUp) {
+                _pickUpTimeController.text = formattedTime;
+              } else {
+                _dropOffTimeController.text = formattedTime;
+              }
+            });
+          },
+        );
+      },
+    );
+  }
+
+  String _formatDate(DateTime? date) {
+    if (date == null) return 'Select date';
+    return "${date.year}-${date.month}-${date.day}";
+  }
+
+  String _formatTime(TimeOfDay? time) {
+    if (time == null) return 'Select time';
+    return time.format(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     final branchModelData = context.tabNavigator.popData;
@@ -49,7 +110,7 @@ class _CarHireBodyState extends State<CarHireBody> {
                       hintText: 'Pick-Up Location',
                       border: OutlineInputBorder(),
                     ),
-                    enabled: false,
+                    readOnly: true,
                   ),
                 ),
                 const SizedBox(width: 8),
@@ -66,153 +127,92 @@ class _CarHireBodyState extends State<CarHireBody> {
               ],
             ),
           ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      hintText: 'Pick-Up Date',
+                      border: OutlineInputBorder(),
+                    ),
+                    controller:
+                        TextEditingController(text: _formatDate(_pickUpDate)),
+                    readOnly: true,
+                    onTap: () => _selectDate(context, true),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                Expanded(
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      hintText: 'Pick-Up Time',
+                      border: OutlineInputBorder(),
+                    ),
+                    controller: _pickUpTimeController,
+                    readOnly: true,
+                    onTap: () => _selectTime(context, true),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      hintText: 'Drop-Off Date',
+                      border: OutlineInputBorder(),
+                    ),
+                    controller:
+                        TextEditingController(text: _formatDate(_pickUpDate)),
+                    readOnly: true,
+                    onTap: () => _selectDate(context, true),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                Expanded(
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      hintText: 'Drop-Off Time',
+                      border: OutlineInputBorder(),
+                    ),
+                    controller: TextEditingController(
+                      text: _formatTime(_dropOffTime),
+                    ),
+                    readOnly: true,
+                    onTap: () => _selectTime(context, true),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Spacer(),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: context.theme.primaryColor,
+                  foregroundColor: context.theme.colorScheme.onPrimary,
+                ),
+                onPressed: () {
+                  // Handle button press
+                },
+                child: const Text(
+                  'Confirm Booking',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
   }
 }
-
-// import 'package:car_rental_app/core/common/widgets/custom_time_picker.dart';
-// import 'package:car_rental_app/src/car/presentation/widget/car_hire_app_bar.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter/cupertino.dart';
-
-// class CarHireScreen extends StatefulWidget {
-//   const CarHireScreen({super.key});
-
-//   @override
-//   _CarHireScreenState createState() => _CarHireScreenState();
-// }
-
-// class _CarHireScreenState extends State<CarHireScreen> {
-//   final TextEditingController _pickUpLocationController =
-//       TextEditingController();
-//   final TextEditingController _dropOffLocationController =
-//       TextEditingController();
-//   final TextEditingController _pickUpTimeController = TextEditingController();
-//   final TextEditingController _dropOffTimeController = TextEditingController();
-//   DateTime? _pickUpDate;
-//   DateTime? _dropOffDate;
-//   TimeOfDay? _pickUpTime;
-//   TimeOfDay? _dropOffTime;
-
-//   Future<void> _selectDate(BuildContext context, bool isPickUp) async {
-//     final DateTime? pickedDate = await showDatePicker(
-//       context: context,
-//       initialDate: DateTime.now(),
-//       firstDate: DateTime.now(),
-//       lastDate: DateTime(2101),
-//     );
-
-//     if (pickedDate != null) {
-//       setState(() {
-//         if (isPickUp) {
-//           _pickUpDate = pickedDate;
-//         } else {
-//           _dropOffDate = pickedDate;
-//         }
-//       });
-//     }
-//   }
-
-//   void _selectTime(BuildContext context, bool isPickUp) {
-//     showModalBottomSheet(
-//       context: context,
-//       builder: (context) {
-//         return CustomTimePicker(
-//           onTimeSelected: (TimeOfDay selectedTime) {
-//             setState(() {
-//               final formattedTime = selectedTime.format(context);
-//               if (isPickUp) {
-//                 _pickUpTimeController.text = formattedTime;
-//               } else {
-//                 _dropOffTimeController.text = formattedTime;
-//               }
-//             });
-//           },
-//         );
-//       },
-//     );
-//   }
-
-//   String _formatDate(DateTime? date) {
-//     if (date == null) return 'Select date';
-//     return "${date.year}-${date.month}-${date.day}";
-//   }
-
-//   String _formatTime(TimeOfDay? time) {
-//     if (time == null) return 'Select time';
-//     return time.format(context);
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: const CarHireAppBar(),
-//       body: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.start,
-//           children: [
-//             TextField(
-//               controller: _pickUpLocationController,
-//               decoration: const InputDecoration(labelText: 'Pick-Up Location'),
-//             ),
-//             TextField(
-//               controller: _dropOffLocationController,
-//               decoration: const InputDecoration(labelText: 'Drop-Off Location'),
-//             ),
-//             Row(
-//               children: [
-//                 Expanded(
-//                   child: TextField(
-//                     decoration:
-//                         const InputDecoration(labelText: 'Pick-Up Date'),
-//                     controller: TextEditingController(
-//                       text: _formatDate(_pickUpDate),
-//                     ),
-//                     readOnly: true,
-//                     onTap: () => _selectDate(context, true),
-//                   ),
-//                 ),
-//                 Expanded(
-//                   child: TextField(
-//                     controller: _pickUpTimeController,
-//                     onTap: () => _selectTime(context, true),
-//                     readOnly: true,
-//                   ),
-//                 ),
-//               ],
-//             ),
-//             Row(
-//               children: [
-//                 Expanded(
-//                   child: TextField(
-//                     decoration:
-//                         const InputDecoration(labelText: 'Drop-Off Date'),
-//                     controller: TextEditingController(
-//                       text: _formatDate(_dropOffDate),
-//                     ),
-//                     readOnly: true,
-//                     onTap: () => _selectDate(context, false),
-//                   ),
-//                 ),
-//                 Expanded(
-//                   child: TextField(
-//                     decoration:
-//                         const InputDecoration(labelText: 'Drop-Off Time'),
-//                     controller: TextEditingController(
-//                       text: _formatTime(_dropOffTime),
-//                     ),
-//                     readOnly: true,
-//                     onTap: () => _selectTime(context, false),
-//                   ),
-//                 ),
-//               ],
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
